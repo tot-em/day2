509a9e6025b22adb3f64dbf6a629acde05dc2e94
Fafa <floor.leaves@gmail.com>
2023-08-11 15:28:50 +0300

    initial push

---
+++ api/server.js
@@ -0,0 +1,18 @@
+// Importing Modules
+const express = require('express')
+
+// Instance
+const app = express()
+
+const port = 3000
+
+app.listen(port, ()=>{
+    console.log("SERVER RUNNING ON PORT: 3000")
+})
+
+//create repo(form3), add collaborator, push to github
+//research objects in javascript and ip address and port no.
+
+/**
+ * 
+ */
